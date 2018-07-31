30312a8d3c929d205ef1a88d49e96cf75c44a38b
Eran Kohen <erankohen@hotmail.com>
2018-07-31 09:28:38 -0400

    keywords

--- secure/config.php
+++ secure/config.php
@@ -9,10 +9,10 @@ define('RECAPTCHA_SECRET','6Le4dh8TAAAAAOkR9RbJCtUZfX6ux2WpZ7TD8F79');
 define('RECAPTCHA_KEY','6Le4dh8TAAAAAAgTlWU8jjntiHDYShEskliSxCW-');
 
 /* HTML Variables */
-define('TITLE','MUNDP 2018');
-define('DESCRIPTION','MUNDP is an annual MUN conference organized by The Koç School. This year we will be hosting 18th session of MUNDP.');
+define('TITLE','MUNDP 2019');
+define('DESCRIPTION','MUNDP is an annual MUN conference organized by The Koç School. This year we will be hosting 19th session of MUNDP.');
 define('AUTHOR','Berk Alp Yakıcı');
-define('KEYWORDS','Model United Nations Development Programme 2018, UNDP, MUNDP, MUNDP\'18, Koc School, Koç School, MODELUNDP, VKV Koç Özel Lisesi, Model Birleşmiş Milletler Geliştirme Programı, Birleşmiş Milletler Geliştirme Programı');
+define('KEYWORDS','Model United Nations Development Programme 2019, UNDP, MUNDP, MUNDP\'19, Koc School, Koç School, MODELUNDP, VKV Koç Özel Lisesi, Model Birleşmiş Milletler Geliştirme Programı, Birleşmiş Milletler Geliştirme Programı');
 
 
 $regisetting['historical'] = false;
