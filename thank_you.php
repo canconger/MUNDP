5e9bc96b66b86b909f7e6fdd91726c6276d81d1a
Eran Kohen <erankohen@hotmail.com>
2018-09-01 23:26:21 +0300

    thank you page

---
+++ thank_you.php
@@ -0,0 +1,43 @@
+<? include("secure/loader.php"); ?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="<?= DESCRIPTION ?>">
+    <meta name="author" content="<?= AUTHOR ?>">
+    <meta name="keywords" content="<?= KEYWORDS ?>">
+    <link rel="icon" href="favicon.ico">
+
+    <title><?= TITLE ?>: Thank You</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+  <body>
+
+    <? include("_includes/navigation.php"); ?>
+
+    <div class="jumbotron executive">
+        <div class="container">
+            <h1>Thank You</h1>
+            <h3>Your application has been received.</h3>
+        </div>
+    </div>
+
+    <? include("_includes/footer.php"); ?>
+
+  </body>
+</html>
