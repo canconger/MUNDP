0ec79d2de1eede82a8200449de1b7e640a1c1d24
Eran Kohen <erankohen@hotmail.com>
2018-09-06 20:42:07 +0300

    weird config issue

--- secure/config.php
+++ secure/config.php
@@ -10,15 +10,9 @@ define('RECAPTCHA_KEY','6Le4dh8TAAAAAAgTlWU8jjntiHDYShEskliSxCW-');
 
 /* HTML Variables */
 define('TITLE','MUNDP 2019');
-<<<<<<< HEAD
-<<<<<<< HEAD
 define('DESCRIPTION','MUNDP is an annual MUN conference organized by The Koç School. This year we will be hosting 19th session of MUNDP.');
-=======
 define('DESCRIPTION','MUNDP is an annual MUN conference organized by The Koç School. This year we will be hosting the 19th session of MUNDP.');
->>>>>>> f5697945cec007bbd8110e204ce540eddced5cbc
-=======
 define('DESCRIPTION','MUNDP is an annual MUN conference organized by The Koç School. This year we will be hosting the 19th session of MUNDP.');
->>>>>>> f5697945cec007bbd8110e204ce540eddced5cbc
 define('AUTHOR','Berk Alp Yakıcı');
 define('KEYWORDS','Model United Nations Development Programme 2019, UNDP, MUNDP, MUNDP\'19, Koc School, Koç School, MODELUNDP, VKV Koç Özel Lisesi, Model Birleşmiş Milletler Geliştirme Programı, Birleşmiş Milletler Geliştirme Programı');
 
