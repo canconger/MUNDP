4db00a40ad21499cee4816fbeb108fdb58d0b355
Eran Kohen <publications@modelundp.org>
2019-07-07 13:44:43 -0700

    fix

--- home.php
+++ home.php
@@ -318,11 +318,11 @@
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
-    <!--
+
     <script src="js/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/sweetalert.min.js"></script>
-
+<!--
     <script>
     $(document).ready(function(){
       swal({
